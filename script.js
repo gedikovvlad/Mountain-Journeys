a5c2fbd37565d2b67e9ec7da021531c7c47dd8fe
gedikovvlad <gedikovvlad@gmail.com>
2023-09-23 21:01:53 +0300

    final ig

--- script.js
+++ script.js
@@ -2,3 +2,37 @@ function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
   }
  
+
+
+  document.getElementById("myVid").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid").addEventListener("mouseleave", function() {
+	this.pause();
+}); 
+
+
+document.getElementById("myVid2").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid2").addEventListener("mouseleave", function() {
+	this.pause();
+}); 
+
+document.getElementById("myVid3").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid3").addEventListener("mouseleave", function() {
+	this.pause();
+}); 
+
+document.getElementById("myVid4").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid4").addEventListener("mouseleave", function() {
+	this.pause();
+}); 
